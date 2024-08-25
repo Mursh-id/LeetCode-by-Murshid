981f8f406602cff1496bcd4813e81b2161acbe77
Murshid P <123350039+Mursh-id@users.noreply.github.com>
2024-08-25 23:37:02 +0530

    Time: 58 ms (74.48%), Space: 50.4 MB (54.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,19 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// brute force solution
 var twoSum = function(nums, target) {
     
-    for ( let i = 0 ; i < nums.length - 1 ; i++ ) {
-        for ( let j = i+1  ; j < nums.length ; j++  ){
-            
-            if( nums[i] + nums[j] == target ){
-                return [i,j]
-            }
-            
-            
-        }
-        
-        
-    }
+    let obj = {}
     
+    for ( let i = 0 ; i < nums.length ; i++ ) {
+        let diff = target - nums[i]
+        if(diff in obj){
+            return [ obj[diff] , i ]
+        }
+        obj[ nums[i] ] = i
+    }
 };

a09d031e89bd8bccfae3179f6fe5e0f5129cb887
Murshid P <123350039+Mursh-id@users.noreply.github.com>
2024-09-03 00:46:58 +0530

    Time: 130 ms (22.58%), Space: 63.4 MB (38.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,28 +1,33 @@
 /**
- * @param {string[]} strs
- * @return {string[][]}
+ * @param {string[]} strs - Array of strings to group into anagrams
+ * @return {string[][]} - Array of arrays, where each sub-array contains anagrams
  */
 var groupAnagrams = function(strs) {
     
-    let data = {}
+    // Initialize an empty object to store grouped anagrams
+    let data = {};
     
-    for (let i =0 ; i < strs.length ; i++){
+    // Iterate over each string in the input array
+    for (let i = 0; i < strs.length; i++) {
         
-        let key = strs[i].split('').sort().join('')
+        // Sort the characters of the current string to create a key
+        // This ensures all anagrams have the same key
+        let key = strs[i].split('').sort().join('');
         
-        if (key in data){
-            data[key] = [...data[key],strs[i]]
+        // If the key already exists in the data object,
+        // append the current string to the corresponding array
+        if (key in data) {
+            data[key] = [...data[key], strs[i]];
+        } else {
+            // If the key doesn't exist, create a new key-value pair
+            // The key is the sorted string, and the value is an array containing the current string
+            data[key] = [strs[i]];
         }
-        else{
-            data[key] = [strs[i]]
-            
-        }
-        
-        
     }
     
-    console.log( Object.values(data) )
+    // Log the values of the data object to see the grouped anagrams
+    console.log(Object.values(data));
     
-    return Object.values(data)
-
+    // Return the values of the data object, which are arrays of anagrams
+    return Object.values(data);
 };

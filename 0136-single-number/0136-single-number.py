4f4df56eedb1082720ca8ad1505c4b22df513b0a
Murshid P <123350039+Mursh-id@users.noreply.github.com>
2024-09-03 23:16:00 +0530

    Time: 126 ms (16.79%), Space: 19.2 MB (9.88%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,17 +1,18 @@
-
-
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
         
-        for i in nums:
-            res = res ^ i
-            
-        return res
+        data = {}
         
-        
-        
-            
+        for i in range(len(nums)):
+            if nums[i] in data:
+                data[nums[i]] = data[nums[i]]+1
+            else:
+                data[nums[i]] = 1
+                
+        print(data)
+        for key, value in data.items():
+            if value == 1:
+                return key
 
             
             
